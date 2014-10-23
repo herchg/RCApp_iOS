ce557acfa6d777f898c1948545bce3e34d2258bf
Lucas Le <lucas_le@wistron.com>
2014-10-23 10:44:06 +0800

    add CacheManager

---
+++ wilib/core/cache/CacheManager.h
@@ -0,0 +1,13 @@
+//
+//  CacheManager.h
+//  RCApp_IOS
+//
+//  Created by Samuel on 2014/10/23.
+//  Copyright (c) 2014年 T3RetailCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CacheManager : NSObject
+
+@end
---
+++ wilib/core/cache/CacheManager.m
@@ -0,0 +1,13 @@
+//
+//  CacheManager.m
+//  RCApp_IOS
+//
+//  Created by Samuel on 2014/10/23.
+//  Copyright (c) 2014年 T3RetailCloud. All rights reserved.
+//
+
+#import "CacheManager.h"
+
+@implementation CacheManager
+
+@end
